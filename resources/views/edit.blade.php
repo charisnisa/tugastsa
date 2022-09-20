c05bf51f0a0e58a3e94deed2137a9ae96c1ebc9f
charisnisa <112613355+charisnisa@users.noreply.github.com>
2022-09-20 10:32:01 +0700

    fromHandlingTSA

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <body>
+        <form method="POST" action="{{url('mahasiswa/'.$id)}}" enctype="multipart/form-data">
+            @csrf
+            {!! method_field('PUT') !!}
+        Username <input type="text" name="username" required value="{{$data->username}}"><br>
+        Nama <input type="text" name="nama" required value="{{$data->nama}}"><br>
+        Email <input type="email" name="email" required value="{{$data->email}}"><br>
+        Password <input type="password" name="password" required value="{{$data->password}}"><br>
+
+
+        @if($data->avatar)
+            <img src="{{ asset('storage/'.$data->avatar) }}" alt="" width="100px"><br>
+        @endif
+
+        File <input type="file" name="berkas" required accept=".jpg,.png">
+        <button type="submit">Simpan</button>
+
+        @if(isset($error))
+            {{ $error }}
+        @endif
+        </form>    
+    </body>
+</html>
